595f04a2af1b29c9b11fad983fc877b3c8d1e41d
zhangchanghai <906393745@qq.com>
2018-01-03 17:02:36 +0800

    添加feign 获取token等

--- src/main/java/com/wechat/WeChatApplication.java
+++ src/main/java/com/wechat/WeChatApplication.java
@@ -2,8 +2,12 @@ package com.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+import com.wechat.util.FeignUtil;
 
 @SpringBootApplication
+@EnableFeignClients(clients = {FeignUtil.class})
 public class WeChatApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/wechat/util/FeignUtil.java
@@ -0,0 +1,28 @@
+package com.wechat.util;
+
+import java.util.Map;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@FeignClient(url="https://api.weixin.qq.com")
+public interface FeignUtil {
+	/**
+	 * 获取token
+	 * @return
+	 */
+	@RequestMapping(value = "/cgi-bin/token?grant_type=client_credential&appid={appid}&secret={secret}",method=RequestMethod.GET)
+	public Map<String,String> getToken(@PathVariable("appid") String appid,@PathVariable("secret") String secret);
+	/**
+	 * 获取用户信息
+	 * @param token
+	 * @param openid
+	 * @return
+	 */
+	@RequestMapping(value="/cgi-bin/user/info?access_token={token}&openid={openid}&lang=zh_CN",method = RequestMethod.GET)
+	public Map<String,String> getUserInfo(@PathVariable("token") String token,@PathVariable("openid") String openid);
+	
+}
