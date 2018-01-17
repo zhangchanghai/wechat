b49ff6bc0932891f2ee80a73c29965fbed5bb69c
zhangchanghai <906393745@qq.com>
2018-01-17 19:48:24 +0800

    更改value的序列化方式

--- src/main/java/com/wechat/config/RedisConfig.java
+++ src/main/java/com/wechat/config/RedisConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration  
 
@@ -15,7 +20,16 @@ public class RedisConfig extends CachingConfigurerSupport {
 	        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
 	        redisTemplate.setConnectionFactory(redisConnectionFactory);
 	        redisTemplate.setKeySerializer(new StringRedisSerializer());
-	        redisTemplate.setValueSerializer(new StringRedisSerializer());
-	        return redisTemplate;
+	        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);  
+	        ObjectMapper om = new ObjectMapper();  
+	        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);  
+	        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);  
+	        jackson2JsonRedisSerializer.setObjectMapper(om);  
+	  
+	        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);  
+	        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);  
+	        redisTemplate.afterPropertiesSet();  
+	  
+	        return redisTemplate;  
 	    }
 }
